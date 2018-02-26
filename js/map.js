3eaa08af4773e6145715535f107d976cc8f9011e
mkeller3 <michaelkeller03@gmail.com>
2018-02-26 10:24:59 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -23,7 +23,7 @@
  			var polyCQL = polyStringParse.replace(regExpPattern, '$2 $1');
  			var polyCQLArray = polyStringParse.split(',')
 
- 			var polyurl = "http://18.221.74.167:8080/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=postgresql:addresses&outputFormat=application/json&format_options=callback:getJson&SrsName=EPSG:4326&cql_filter=INTERSECTS(geom, POLYGON((" + polyCQL + ', ' + polyCQLArray[1] + ' ' + polyCQLArray[0] + ")))"; var download_url = "http://localhost:8080/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=policy:rclt_sv_allpersonal&outputFormat=csv&format_options=callback:getJson&SrsName=EPSG:4326&cql_filter=INTERSECTS(geom, POLYGON((" + polyCQL + ', ' + polyCQLArray[1] + ' ' + polyCQLArray[0] + ")))";
+ 			var polyurl = location.protocol + "//18.221.74.167:8080/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=postgresql:addresses&outputFormat=application/json&format_options=callback:getJson&SrsName=EPSG:4326&cql_filter=INTERSECTS(geom, POLYGON((" + polyCQL + ', ' + polyCQLArray[1] + ' ' + polyCQLArray[0] + ")))"; var download_url = "http://localhost:8080/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=policy:rclt_sv_allpersonal&outputFormat=csv&format_options=callback:getJson&SrsName=EPSG:4326&cql_filter=INTERSECTS(geom, POLYGON((" + polyCQL + ', ' + polyCQLArray[1] + ' ' + polyCQLArray[0] + ")))";
 
 			$.ajax({url: polyurl,
  				success: function(result){
